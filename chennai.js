a1228996311fe846f96577a0a6cda261839720e4
Rudra Shekhar Nag <82301307+Rudra-2009@users.noreply.github.com>
2023-02-05 18:17:32 +0530

    Add files via upload

---
+++ chennai.js
@@ -0,0 +1,55 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+function homeKol() {
+location.replace("index.html");
+}
+
+
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,20 @@
+function french() {
+    document.getElementById("heading").innerHTML="Blog de voyage en Inde";
+    document.getElementById("paragraph").innerHTML="Voici quelques villes en Inde. Cliquez sur l'un d'entre eux pour obtenir des informations à son sujet";
+}
+function english() {
+    document.getElementById("heading").innerHTML="India Travel Blog";
+    document.getElementById("paragraph").innerHTML="Here are a few cities in India.Click on one to gain some info about it ";
+}
+function kolkata() {
+    location.replace("kolkata.html");
+}
+function mumbai() {
+    location.replace("mumbai.html");
+}
+function delhi() {
+    location.replace("delhi.html");
+}
+function chennai() {
+    location.replace("chennai.html");
+}
